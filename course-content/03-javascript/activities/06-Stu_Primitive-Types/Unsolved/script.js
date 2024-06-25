829b90ed92d5198299513f17a4b0204204d9b470
Redwolf917 <xander0429@gmail.com>
2024-06-24 19:29:00 -0600

    initial commit

---
+++ course-content/03-javascript/activities/06-Stu_Primitive-Types/Unsolved/script.js
@@ -0,0 +1,35 @@
+const one = 64;
+const two = '656302';
+const three = false;
+const four = null;
+let five = 64.55;
+let six = 'Howdy!';
+let seven;
+
+// Insert comments to explain what each console log below will log to the console
+// this will log a number
+console.log(typeof one);
+// this will log a string
+console.log(typeof two);
+//this will be a boolean
+console.log(typeof three);
+//so this will print an object (but it won't)
+console.log(typeof four);
+//this will print a number
+console.log(typeof five);
+//this will print a string
+console.log(typeof six);
+//this will print undefined
+console.log(typeof seven);
+
+five = 'Hello!';
+six = false;
+seven = 23;
+
+// Insert comments to explain what each console log below will log to the console
+//this will print a string
+console.log(typeof five);
+//this will print a boolean
+console.log(typeof six);
+//this wil print a number
+console.log(typeof seven);
